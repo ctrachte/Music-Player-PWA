4fab7d180259035d1e5f52bb62381a6253439b6d
ctrachte <cetrachte@hotmail.com>
2018-02-28 11:55:21 -0600

    add landing and library component to main

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ class App extends Component {
           <h1>Bloc Jams</h1>
         </header>
         <main>
+          <Route exact path="/" component={Landing} />
+          <Route path="/library" component={Library} />
         </main>
       </div>
     );
